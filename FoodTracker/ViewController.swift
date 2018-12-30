4e8d7b1d200e5ce539d76918cf19a8b17ed27782
Tony Hu <tonyhu4156@gmail.com>
2018-12-30 13:27:58 -0800

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Tony Hu on 12/30/18.
+//  Copyright © 2018 Tony Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
